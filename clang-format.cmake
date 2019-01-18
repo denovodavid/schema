49b241dda58e929f0acdcdc6cc4920d38235f7f5
David J Holland <stooge213@hotmail.com>
2019-01-18 14:49:52 +1100

    :construction_worker: add clang-format cmake target

---
+++ clang-format.cmake
@@ -0,0 +1,16 @@
+# https://arcanis.me/en/2015/10/17/cppcheck-and-clang-format
+
+# additional target to perform clang-format run, requires clang-format
+
+# get source files
+file(GLOB_RECURSE ALL_SOURCE_FILES
+     "include/*.h"
+     "src/*.cpp"
+     "sandbox/*.h"
+     "sandbox/*.cpp")
+
+add_custom_target(clangformat
+                  COMMAND clang-format
+                  -style=google
+                  -i
+                  ${ALL_SOURCE_FILES})
