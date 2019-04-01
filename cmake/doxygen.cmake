dded8244f3d310674484ce5cae6455e3fc97eced
David J Holland <stooge213@hotmail.com>
2019-04-01 14:11:25 +1100

    :wrench: fix docs having html folder

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -10,11 +10,16 @@ if (DOXYGEN_FOUND)
     # request to configure the file
     configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
 
+    # build docs
     add_custom_target(doxygen
                       COMMAND ${DOXYGEN_EXECUTABLE} ${DOXYGEN_OUT}
                       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
                       COMMENT "Generating API documentation with Doxygen"
                       VERBATIM)
+
+    # move html folder to docs
+    add_custom_command(TARGET doxygen
+                       COMMAND ${CMAKE_COMMAND} -E rename ${CMAKE_CURRENT_BINARY_DIR}/doxygen-docs/html ${CMAKE_CURRENT_SOURCE_DIR}/docs)
 else (DOXYGEN_FOUND)
   message("Doxygen needs to be installed to generate documentation")
 endif (DOXYGEN_FOUND)
